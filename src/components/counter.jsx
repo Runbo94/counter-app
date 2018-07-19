b9524711ac38f23dd60c37bbe3a245f349aa0798
Runbo Zhao <rzhao03@syr.edu>
2018-07-19 15:57:17 -0400

    Conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There is no tags!</p>;
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
