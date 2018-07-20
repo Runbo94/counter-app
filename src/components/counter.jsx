5887deaf73fbc50872bcf6333c97efa86a418249
Runbo Zhao <rzhao03@syr.edu>
2018-07-20 12:21:39 -0400

    Revert "Conditional rendering"
    This reverts commit b9524711ac38f23dd60c37bbe3a245f349aa0798.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,22 +2,20 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: 0
   };
-
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There is no tags!</p>;
-    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-  }
-
   render() {
     return (
       <React.Fragment>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
