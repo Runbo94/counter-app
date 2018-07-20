8271ed68d601af8f3de5ccca6b56d0510ef00afc
Runbo Zhao <rzhao03@syr.edu>
2018-07-20 12:36:55 -0400

    handling events and binding event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,15 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 1
   };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "m-2 badge badge-sm badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
