197031257b5fefa9867e09f5c8898dc4d7df2bb4
Runbo Zhao <rzhao03@syr.edu>
2018-07-21 15:53:43 -0400

    Passing children

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
+    value: this.props.value
   };
 
   //   constructor() {
@@ -12,12 +12,13 @@ class Counter extends Component {
 
   handleIncrement = () => {
     console.log("Increment Clicked", this);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ count: this.state.value + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
@@ -32,13 +33,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "m-2 badge badge-sm badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
